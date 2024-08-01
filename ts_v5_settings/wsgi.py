b754a8c011f2c04bf6a2db5e2ee19fe2d127046e
Daniel Tebar <dtebar@gmail.com>
2024-08-01 02:18:41 -0400

    Your descriptive commit message

--- ts_v5_settings/wsgi.py
+++ ts_v5_settings/wsgi.py
@@ -1,7 +1,14 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
+import dj_database_url
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ts_v5_app.settings')
+from ts_v5_settings.settings import DATABASES
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ts_v5_project.settings')  # Replace with your project name
 
 application = get_wsgi_application()
+
+# Heroku Database Configuration
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
